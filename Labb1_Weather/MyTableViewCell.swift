e6c4f3890f9a3a6872b0bb46bad9822c40522a22
Petteri <Buggis123buggis@gmail.com>
2018-03-16 13:28:38 +0100

    initial commit + tableview start

---
+++ Labb1_Weather/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  Labb1_Weather
+//
+//  Created by lösen är 0000 on 2018-03-16.
+//  Copyright © 2018 PT. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
