8f1b03c2c22aa062bc3b9b1dbc527598e167b5f2
Petteri <Buggis123buggis@gmail.com>
2018-03-17 11:39:51 +0100

    Json Data recived

---
+++ Labb1_Weather/Cities.swift
@@ -0,0 +1,13 @@
+//
+//  Cities.swift
+//  Labb1_Weather
+//
+//  Created by lösen är 0000 on 2018-03-17.
+//  Copyright © 2018 PT. All rights reserved.
+//
+
+import UIKit
+
+class Cities: NSObject {
+
+}
